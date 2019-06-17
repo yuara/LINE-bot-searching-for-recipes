630ab63871135ef4785b5cff4d0e3f6506d2de70
Yu Arakawa <yuarakawa@Yus-MacBook.local>
2019-06-17 08:33:26 -0700

    changed

--- serch.py
+++ serch.py
@@ -1,26 +1,38 @@
 import json
 import requests
 import os
+import random
 
 applicationId = os.environ["LINE_GOHAN_RAKUTEN_RECIPE_ID"]
 
 url_category = "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&formatVersion=2&elements=categoryName%2CcategoryId%2CparentCategoryId&categoryType=medium&applicationId=" + applicationId
 
-url_recipe = lambda categoryId: "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&formatVersion=2&categoryId=" + categoryId + "&applicationId=" + applicationId
+url_ranking = lambda categoryId: "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&formatVersion=2&categoryId=" + categoryId + "&applicationId=" + applicationId
 
 
 def getMenus(word):
+
     id_imfor = requests.get(url_category).json()
     id_imfor_list = id_imfor["result"]["medium"]
+
+    result_list = []
+
     for id in id_imfor_list:
-        if id["categoryName"] == word:
+        if word in id["categoryName"]:
             categoryId = str(id["parentCategoryId"]) + "-" + str(id["categoryId"])
-            break
 
-    url = url_recipe(categoryId)
-    recipe_list = []
+            url = url_ranking(categoryId)
 
-    re = requests.get(url).json()
-    result = re["result"][0]["recipeTitle"] + "\n" + re["result"][0]["recipeUrl"]
+            recipe_imfor = requests.get(url).json()
+            recipe_imfor_list = recipe_imfor["result"]
+
+            for recipe_ranking in recipe_imfor_list:
+
+                result = recipe_imfor_list[recipe_ranking]["recipeTitle"] + "\n" + recipe_imfor_list[recipe_ranking]["recipeUrl"]
+
+                result_list.append(result)
+                
+
+    result =  random.choice(result_list)
 
     return result
